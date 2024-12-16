d03d6ead0dd74fbc87100844c2828032bf3c181f
sunilkommiri <sunil1kommuri@gmail.com>
2024-12-16 12:28:59 -0600

    Update security-group.tf

--- security-group.tf
+++ security-group.tf
@@ -1,28 +1,32 @@
-provider "aws"{
-region = "us-east-1"
+provider "aws" {
+  region = "us-east-1"
 }
-resource = "aws_security_group" "first_SG"{
-vpc_id = aws_vpc.tf.id
 
-ingress {
-from_port = 80
-to_port = 80
-protocol = "tcp"
-cidr_blocks = ["0.0.0.0/0"]
-}
-ingress {
-from_port = 22
-to_port = 22
-protocol ="tcp"
-cidr_blocks = ["0.0.0.0/0"]
-}
-egress {
-from_port = 0
-to_port = 0
-prot0col = "-1"
-cidr_blocks = ["0.0.0.0/0"]
-}
-tags = {
-Name = "First_SG"
-}
+resource "aws_security_group" "first_SG" {
+  vpc_id = aws_vpc.First_vpc.id  # Correcting the reference to the VPC
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"  # Corrected the typo in "protocol"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "First_SG"
+  }
 }
