47c46a6c1cb386cbf0c802c443a90057055867db
sunilkommiri <sunil1kommuri@gmail.com>
2025-01-20 13:57:50 -0600

    Create main.tf

---
+++ ASG/main.tf
@@ -0,0 +1,24 @@
+#provider "aws"
+#region = var.region
+
+resource "aws_autoscaling_group" "first_ASG" {
+launch_template {
+id = aws_launch_template.first_template.id
+version ="$latest"
+}
+vpc_zone_identifier = var.subnet_ids
+min_size = var.min_size
+max_size = var.max_size
+desired_capacity =var.desired_capacity
+health_check_type= "EC2"
+health_check_grace_period =300
+ tag {
+    key                 = "Name"
+    value               = "example-autoscaling-instance"
+    propagate_at_launch = true
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
