0ddf5d39305f62bee184b16628fb95c4abbf1f8b
sunilkommiri <sunil1kommuri@gmail.com>
2025-03-12 14:46:29 -0500

    Update SecurityGroup.tf

--- ELB/SecurityGroup.tf
+++ ELB/SecurityGroup.tf
@@ -18,7 +18,12 @@ resource "aws_security_group" "first_SG" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
+ingress {
+from_port= 8080
+to_port= 8080
+protocol = "tcp"
+cidr_blocks = ["0.0.0.0/0"]
+{
   egress {
     from_port   = 0
     to_port     = 0
